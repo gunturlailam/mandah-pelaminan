1ec2cb29e05c08e1969f8ed75b8da7e38e0ef5ee
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-21 15:44:46 +0700

    feat: menambahkan migrate pegawai

---
+++ app/Database/Migrations/2025-10-21-084137_Pegawai.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Pegawai extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_pegawai' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'nama_pegawai' => ['type' => 'VARCHAR', 'constraint' => 100],
+            'jabatan' => ['type' => 'VARCHAR', 'constraint' => 50],
+            'username' => ['type' => 'VARCHAR', 'constraint' => 50, 'unique' => true],
+            'password' => ['type' => 'VARCHAR', 'constraint' => 255],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id_pegawai', true);
+        $this->forge->createTable('pegawai');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pegawai');
+    }
+}
