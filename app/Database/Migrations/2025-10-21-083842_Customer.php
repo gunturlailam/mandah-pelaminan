9f380af1351f21807f3990e8baef828410b505ea
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-21 15:41:26 +0700

    feat: menambahkan migrate customer

--- app/Database/Migrations/2025-10-21-083842_Customer.php
+++ app/Database/Migrations/2025-10-21-083842_Customer.php
@@ -8,11 +8,22 @@ class Customer extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_customer' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'nama_customer' => ['type' => 'VARCHAR', 'constraint' => 100],
+            'alamat' => ['type' => 'TEXT'],
+            'no_hp' => ['type' => 'VARCHAR', 'constraint' => 20],
+            'email' => ['type' => 'VARCHAR', 'constraint' => 100, 'null' => true],
+            'wilayah_id' => ['type' => 'INT', 'unsigned' => true],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id_customer', true);
+        $this->forge->addForeignKey('wilayah_id', 'wilayah', 'id_wilayah', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('customer');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('customer');
     }
 }
