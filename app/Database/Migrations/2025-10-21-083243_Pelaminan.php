712d0a75001a1ff5555ecf37b531bfecab040860
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-21 15:37:43 +0700

    feat: menambahkan migrate pelaminan

--- app/Database/Migrations/2025-10-21-083243_Pelaminan.php
+++ app/Database/Migrations/2025-10-21-083243_Pelaminan.php
@@ -8,11 +8,46 @@ class Pelaminan extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_pelaminan' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_kategori' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'nama_pelaminan' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'deskripsi' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'harga_sewa' => [
+                'type' => 'DOUBLE',
+            ],
+            'stok' => [
+                'type' => 'INT',
+                'default' => 0,
+            ],
+            'foto' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id_pelaminan', true);
+        $this->forge->addForeignKey('id_kategori', 'kategori_pelaminan', 'id_kategori', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('pelaminan');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('pelaminan');
     }
 }
