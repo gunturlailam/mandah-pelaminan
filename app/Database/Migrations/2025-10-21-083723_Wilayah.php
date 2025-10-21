5963247714f270f2eccacf4b6ad53cad135db599
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-21 15:39:01 +0700

    feat: menambahkan migrate  wilayah

--- app/Database/Migrations/2025-10-21-083723_Wilayah.php
+++ app/Database/Migrations/2025-10-21-083723_Wilayah.php
@@ -8,11 +8,17 @@ class Wilayah extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_wilayah' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'nama_wilayah' => ['type' => 'VARCHAR', 'constraint' => 100],
+            'ongkir' => ['type' => 'DOUBLE', 'default' => 0],
+        ]);
+        $this->forge->addKey('id_wilayah', true);
+        $this->forge->createTable('wilayah');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('wilayah');
     }
 }
